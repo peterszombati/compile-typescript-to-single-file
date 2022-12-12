a63a549e2862b61f3b2737626d8fcae6fe7e3543
Peter Szombati <szombatipeti7@gmail.com>
2022-12-12 01:18:22 +0100

    .

---
+++ build.sh
@@ -0,0 +1,1 @@
+tsc --moduleResolution node --module amd --target "esnext" --skipLibCheck true --out build.js ./src/index.ts && node compile.js
---
+++ compile.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+
+const data = fs.readFileSync(path.join(__dirname, 'build.js'))
+const before = Buffer.from(`const ___defined = {}
+let ___exports = {}
+`)
+const after = Buffer.from(`
+function ___require(name) {
+    if (!___defined[name]) {
+        ___defined[name] = require(name)
+    }
+    return ___defined[name]
+}
+function define(target, functions, callback) {
+    ___defined[target] = {}
+    ___exports = ___defined[target]
+    callback(___require, ___defined[target], ...functions.slice(2).map(i => {
+        if (!___defined[i]) {
+            ___defined[i] = require(i)
+        }
+        return ___defined[i]
+    }))
+}
+module.exports = ___exports`)
+fs.writeFileSync(path.join(__dirname, 'build.js'), Buffer.concat([before, data, after]))
